7eb0cbc7119509456d539ab1e9a70d51fb6fd18e
dzzxjl <dzzxjl@gmail.com>
2025-07-02 23:29:52 +0800

    [feat] update

--- mammut-spark/src/main/java/io/github/dzzxjl/spark/Demo.java
+++ mammut-spark/src/main/java/io/github/dzzxjl/spark/Demo.java
@@ -10,19 +10,24 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        SparkConf conf = new SparkConf().setAppName("HelloWorld").setMaster("local").setSparkHome("/usr/lib/spark/spark-2.1.1-bin-hadoop2.7");
-        // setMaster指定Master
-        // setSparkHome指向安装spark的地址，视环境而定
-        JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> data = sc.textFile("README.md");
-        // 加载README.md文件并创建RDD
-        data.foreach(new VoidFunction<String>() {
-            public void call(String s) throws Exception {
-                System.out.println(s);
-            }
-        });
-        // 输出RDD中的每个分区的内容
+        System.out.println("hello");
+
+
+
+        // SparkConf conf = new SparkConf().setAppName("HelloWorld").setMaster("local").setSparkHome("/usr/lib/spark/spark-2.1.1-bin-hadoop2.7");
+        // // setMaster指定Master
+        // // setSparkHome指向安装spark的地址，视环境而定
+        // JavaSparkContext sc = new JavaSparkContext(conf);
+        //
+        // JavaRDD<String> data = sc.textFile("README.md");
+        // // 加载README.md文件并创建RDD
+        // data.foreach(new VoidFunction<String>() {
+        //     public void call(String s) throws Exception {
+        //         System.out.println(s);
+        //     }
+        // });
+        // // 输出RDD中的每个分区的内容
 
     }
 
